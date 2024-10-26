8bd948d15ca6301548bf5a1514cde2e80c896357
vishalmeena2211 <meenavishal2211@gmail.com>
2024-10-27 00:00:52 +0530

    logo isshue solved

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import logo from '/logo.png';
 
 export function Navbar() {
   return (
     <nav className="flex h-[98px] items-center justify-between px-[20px] text-white md:px-[50px] bg-[#6366F1]">
       <div>
-        <Image src={logo} width={96.67} height={49.03} alt={"ConnectEd"} />
+        <Image src='/logo.png' width={96.67} height={49.03} alt={"ConnectEd"} />
       </div>
       <h1 className="text-center text-xl font-bold leading-tight md:text-[32px]">
         Student Portal
