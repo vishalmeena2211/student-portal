d8d4eb9caf64713691d37c770271f3fdd70c88d0
vishalmeena2211 <meenavishal2211@gmail.com>
2024-10-27 00:06:35 +0530

    using img over Image

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
+
 
 export function Navbar() {
   return (
     <nav className="flex h-[98px] items-center justify-between px-[20px] text-white md:px-[50px] bg-[#6366F1]">
       <div>
-        <Image src='/logo.png' width={96.67} height={49.03} alt={"ConnectEd"} />
+        <img src='/logo.png' width={96.67} height={49.03} alt={"ConnectEd"} />
       </div>
       <h1 className="text-center text-xl font-bold leading-tight md:text-[32px]">
         Student Portal
